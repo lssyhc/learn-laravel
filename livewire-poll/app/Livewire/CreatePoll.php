39be18bd29154df76fc4eb66308579bd7f3c7553
Cahyo Susilo <lssyhc.study@gmail.com>
2025-06-30 21:36:00 +0700

    05: actions in livewire

--- livewire-poll/app/Livewire/CreatePoll.php
+++ livewire-poll/app/Livewire/CreatePoll.php
@@ -7,9 +7,15 @@ use Livewire\Component;
 class CreatePoll extends Component
 {
     public string $title;
+    public array $options = ['First'];
 
     public function render()
     {
         return view('livewire.create-poll');
+    }
+
+    public function addOption()
+    {
+        $this->options[] = '';
     }
 }
