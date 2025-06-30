14a749bfdc03f4e0cfd4ddde7cbca05aafd4bac0
Cahyo Susilo <lssyhc.study@gmail.com>
2025-07-01 02:17:23 +0700

    12: voting in polls

--- livewire-poll/app/Livewire/Polls.php
+++ livewire-poll/app/Livewire/Polls.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use App\Models\Poll;
+use App\Models\Option;
 use Livewire\Attributes\On;
 use Livewire\Component;
 
@@ -13,5 +14,10 @@ class Polls extends Component
     {
         $polls = Poll::with('options.votes')->latest()->get();
         return view('livewire.polls', ['polls' => $polls]);
+    }
+
+    public function vote(Option $option)
+    {
+        $option->votes()->create();
     }
 }
