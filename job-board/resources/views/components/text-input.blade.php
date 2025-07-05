dec86eccbac593d6167126173e70ae122bccec9f
Cahyo Susilo <lssyhc.study@gmail.com>
2025-07-05 23:43:38 +0700

    24: authentication: sign-in form

--- job-board/resources/views/components/text-input.blade.php
+++ job-board/resources/views/components/text-input.blade.php
@@ -7,8 +7,9 @@
             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
         </svg>
     </button>
-    <input x-ref="input-{{$name}}" type="text" placeholder="{{$placeholder}}" name="{{$name}}" value="{{$value}}"
+    @endif
+
+    <input x-ref="input-{{$name}}" type="{{$type}}" placeholder="{{$placeholder}}" name="{{$name}}" value="{{$value}}"
         id="{{$name}}"
         class="w-full rounded-md border-0 py-1.5 px-2.5 pr-8 text-sm ring-1 ring-slate-300 placeholder:text-slate-400 focus:ring-2" />
-    @endif
 </div>
