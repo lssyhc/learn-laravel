3e091e45b75f57e72274fb8a0b3f6d874c25d987
Cahyo Susilo <lssyhc.study@gmail.com>
2025-07-07 22:41:59 +0700

    40: employer: register as an employer

--- job-board/resources/views/components/text-input.blade.php
+++ job-board/resources/views/components/text-input.blade.php
@@ -20,9 +20,9 @@
     <textarea id="{{ $name }}" name="{{ $name }}"
         @class([ 'w-full rounded-md border-0 py-1.5 px-2.5 text-sm ring-1 placeholder:text-slate-400 focus:ring-2'
         , 'pr-8'=> $formRef,
-        'ring-slate-300' => !$errors->has($name),
-        'ring-red-300' => $errors->has($name),
-    ])>{{ old($name, $value) }}</textarea>
+            'ring-slate-300' => !$errors->has($name),
+            'ring-red-300' => $errors->has($name),
+        ])>{{ old($name, $value) }}</textarea>
     @endif
 
     @error($name)
