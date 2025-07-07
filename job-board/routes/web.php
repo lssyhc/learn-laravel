0a0ded428c3c6b7ff49babe273a2ab6f5678df8d
Cahyo Susilo <lssyhc.study@gmail.com>
2025-07-07 23:18:58 +0700

    41: employer: middleware checking for permissions

--- job-board/routes/web.php
+++ job-board/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\EmployerController;
 use App\Http\Controllers\JobApplicationController;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\MyJobApplicationController;
+use App\Http\Controllers\MyJobController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('', fn() => to_route('jobs.index'));
@@ -26,4 +27,6 @@ Route::middleware('auth')->group(function () {
 
     Route::resource('employer', EmployerController::class)
         ->only('create', 'store');
+
+    Route::middleware('employer')->resource('my-job', MyJobController::class);
 });
