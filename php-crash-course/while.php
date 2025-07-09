b333d42c0c8b44a3bd05fb996f2a3a1cb7943b02
Cahyo Susilo <lssyhc.study@gmail.com>
2025-07-09 17:07:50 +0700

    03: loops - while loop (guess the secret game)

---
+++ php-crash-course/while.php
@@ -0,0 +1,20 @@
+<?php
+
+$secret = "magic";
+$attempts = 0;
+$maxAttempts = 5;
+
+while ($attempts < $maxAttempts) {
+    echo "Guess the password: ";
+    $guess = trim(fgets(STDIN));
+    $attempts++;
+
+    if ($guess == $secret) {
+        echo "Correct! You've unlocked the treasure! 💎\n";
+        break;
+    } elseif ($attempts == $maxAttempts) {
+        echo "Out of attemps! The treasure remains locked. 🔒\n";
+    } else {
+        echo "Wrong! Try again. Attempts left: " . ($maxAttempts - $attempts) . "\n";
+    }
+}
