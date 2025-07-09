f220534b5e60fbd836787b6fd1e093b8697a1ea1
Cahyo Susilo <lssyhc.study@gmail.com>
2025-07-10 01:04:06 +0700

    (by ref) 17: performance test: ref vs value - big bata example. is there a difference?

--- php-crash-course/foreach-ref.php
+++ php-crash-course/foreach-ref.php
@@ -5,8 +5,8 @@ $startTime = microtime(true);
 $startMemory = memory_get_usage();
 
 $out = [];
-foreach ($largeArray as $value) {
-    $out[] = $value * 2;
+foreach ($largeArray as &$value) {
+    $value = $value * 2;
 }
 
 $endTime = microtime(true);
