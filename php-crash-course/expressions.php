364d9820919bd2b46492fc8ddacd91b1c06cc5b8
Cahyo Susilo <lssyhc.study@gmail.com>
2025-07-09 15:27:24 +0700

    00: first php expressions

---
+++ php-crash-course/expressions.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "Welcome to PHP\n";
+
+$name = "Lily";
+echo "Hello, " . $name . "!\n";
+
+$pizzas = 3;
+$slicesPerPizza = 8;
+$totalSlices = $pizzas * $slicesPerPizza;
+
+echo "Total pizza slices: " . $totalSlices . "\n";
+
+$isHungry = false;
+echo "Hungry?";
+echo $isHungry ? "Yes" : "No";
+echo "\n";
