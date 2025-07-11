77e24f54cb0f4075ad754b2ee045aaa34ad53761
Cahyo Susilo <lssyhc.study@gmail.com>
2025-07-12 04:00:57 +0700

    39: interfaces (payment processors example)

---
+++ php-crash-course/classes/interfaces.php
@@ -0,0 +1,33 @@
+<?php
+
+interface PaymentProcessor
+{
+    public function processPayment(float $amount): bool;
+    public function refundPayment(float $amount): bool;
+}
+
+abstract class OnlinePaymentProcessor implements PaymentProcessor
+{
+    public function __construct(protected string $apiKey)
+    {
+    }
+
+    public function processPayment(float $amount): bool
+    {
+
+    }
+
+    public function refundPayment(float $amount): bool
+    {
+
+    }
+}
+
+class StripeProcessor extends OnlinePaymentProcessor
+{
+}
+class PaypalProcessor extends OnlinePaymentProcessor
+{
+}
+
+$processor = new StripeProcessor("KEY_");
