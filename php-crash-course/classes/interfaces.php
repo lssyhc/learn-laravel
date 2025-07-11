4dcf731c6619853cff93d36e2d99bf235a02cc37
Cahyo Susilo <lssyhc.study@gmail.com>
2025-07-12 04:42:00 +0700

    40: abstract classes

--- php-crash-course/classes/interfaces.php
+++ php-crash-course/classes/interfaces.php
@@ -12,22 +12,58 @@ abstract class OnlinePaymentProcessor implements PaymentProcessor
     {
     }
 
+    abstract protected function validateApiKey(): bool;
+    // abstract protected function executePayment(): bool;
+    // abstract protected function executeRefund(): bool;
+
     public function processPayment(float $amount): bool
     {
-
+        if (!$this->validateApiKey()) {
+            throw new Exception("Invalid API Key");
+        }
+        // return $this->executePayment($amount);
+        return true;
     }
 
     public function refundPayment(float $amount): bool
     {
-
+        if (!$this->validateApiKey()) {
+            throw new Exception("Invalid API Key");
+        }
+        // return $this->executePayment($amount);
+        return true;
     }
 }
 
 class StripeProcessor extends OnlinePaymentProcessor
 {
+    protected function validateApiKey(): bool
+    {
+        return strpos($this->apiKey, 'sk_') === 0;
+    }
 }
 class PaypalProcessor extends OnlinePaymentProcessor
 {
+    protected function validateApiKey(): bool
+    {
+        return strlen($this->apiKey) === 32;
+    }
 }
 
-$processor = new StripeProcessor("KEY_");
+class CashProcessor implements PaymentProcessor
+{
+    public function processPayment(float $amount): bool
+    {
+        echo "Cash payment...";
+        return true;
+    }
+
+    public function refundPayment(float $amount): bool
+    {
+        echo "Refund payment...";
+        return true;
+    }
+}
+
+$processor = new StripeProcessor("sk_test_1234567890");
+$processor->processPayment(500);
